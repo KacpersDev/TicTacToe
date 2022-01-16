d323204dd93d4391c10db5d7ce71d271b997ebf5
kacper <vkacperpl@gmail.com>
2022-01-16 01:14:35 +0100

    Made 2/4 of stage

---
+++ main.py
@@ -0,0 +1,19 @@
+cells = input()
+
+first_cell = cells[0]
+second_cell = cells[1]
+third_cell = cells[2]
+fourth = cells[3]
+fifth = cells[4]
+sixth = cells[5]
+seventh = cells[6]
+eighth = cells[7]
+ninth = cells[8]
+
+print(f"""
+---------
+| {first_cell} {second_cell} {third_cell} |
+| {fourth} {fifth} {sixth} |
+| {seventh} {eighth} {ninth} |
+---------
+""")
